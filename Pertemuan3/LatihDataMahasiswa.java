30fd339d9e353d6390544afeb447cb5298314c25
khairunnisanailah <d1041241054@student.untan.ac.id>
2026-02-13 08:04:17 +0700

    Pertemuan3

---
+++ Pertemuan3/LatihDataMahasiswa.java
@@ -0,0 +1,19 @@
+package Pertemuan3;
+
+public class LatihDataMahasiswa {
+    public static void main(String[] args) {
+        long nim = 2301010001L;
+        String nama = "Budi Santoso";
+        char kelas = 'A';
+        boolean status = true;
+        double ipk = 3.75;
+        int sks = 72;
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM           : "+nim);
+        System.out.println("Nama          : "+nama);
+        System.out.println("Kelas         : "+kelas);
+        System.out.println("Status Aktif  : "+status);
+        System.out.println("IPK Semester  : "+ipk);
+        System.out.println("Total SKS     : "+sks);
+    }
+}
---
+++ Pertemuan3/LatihPerhitungan.java
@@ -0,0 +1,17 @@
+package Pertemuan3;
+
+public class LatihPerhitungan {
+    public static void main(String[] args) {
+        int saldo = 1000000;
+        System.err.println("=== SIMULASI TRANSAKSI BANK ===");
+        System.err.println("Saldo Awal             : "+saldo);
+        int setoran = 500000;
+        int saldosetelahsetoran = saldo+setoran;
+        System.out.println("Setoran                : "+setoran);
+        System.out.println("Saldo Setelah Setoran  : "+saldosetelahsetoran);
+        int penarikan = 250000;
+        int saldoakhir = saldosetelahsetoran-penarikan;
+        System.out.println("Penarikan              : "+penarikan);
+        System.out.println("Saldo Akhir            : "+saldoakhir);
+    }
+}
