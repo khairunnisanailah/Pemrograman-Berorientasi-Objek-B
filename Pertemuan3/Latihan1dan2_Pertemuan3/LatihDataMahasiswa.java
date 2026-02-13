8391bda5fe7894b72dfa6bd734391f15efa64186
khairunnisanailah <d1041241054@student.untan.ac.id>
2026-02-13 08:20:39 +0700

    LatihDataMahasiswa.java

--- Pertemuan3/Latihan1dan2_Pertemuan3/LatihDataMahasiswa.java
+++ Pertemuan3/Latihan1dan2_Pertemuan3/LatihDataMahasiswa.java
@@ -1,13 +1,19 @@
 package Pertemuan3.Latihan1dan2_Pertemuan3;
-
+/*
+NAMA: KHAIRUNNISA NAILAH SAADAH
+NIM: D1041241054
+TANGGAL: 13/02/2026
+ */
 public class LatihDataMahasiswa {
     public static void main(String[] args) {
-        long nim = 2301010001L;
-        String nama = "Budi Santoso";
-        char kelas = 'A';
+        //deklarasi dan inisialisasi variable
+        long nim = 2301010001L;// menggunakan long karena melebihi int
+        String nama = "Budi Santoso"; //menggunakan string karena ingin menyimpan lebih dari 1 karakter
+        char kelas = 'A'; //menggunakan char karena hanya ingin menyimpan 1 karakter
         boolean status = true;
-        double ipk = 3.75;
+        double ipk = 3.75; // menggunakan double karena ipk memiliki nilai desimal
         int sks = 72;
+         //menampilkan data
         System.out.println("=== DATA MAHASISWA ===");
         System.out.println("NIM           : "+nim);
         System.out.println("Nama          : "+nama);
