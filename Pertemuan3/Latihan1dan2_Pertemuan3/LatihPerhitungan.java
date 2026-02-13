9c97b73507d338d1764fbbfa7a8fe0eb3e8de8b4
khairunnisanailah <d1041241054@student.untan.ac.id>
2026-02-13 08:18:08 +0700

    Pertemuan3

--- Pertemuan3/Latihan1dan2_Pertemuan3/LatihPerhitungan.java
+++ Pertemuan3/Latihan1dan2_Pertemuan3/LatihPerhitungan.java
@@ -1,16 +1,28 @@
 package Pertemuan3.Latihan1dan2_Pertemuan3;
-
+/*
+NAMA: KHAIRUNNISA NAILAH SAADAH
+NIM: D1041241054
+TANGGAL: 13/02/2026
+ */
 public class LatihPerhitungan {
     public static void main(String[] args) {
+        //deklarasi dan inisialisasi variable
         int saldo = 1000000;
+        //menapilkan saldo awal
         System.err.println("=== SIMULASI TRANSAKSI BANK ===");
         System.err.println("Saldo Awal             : "+saldo);
+        //deklarasi dan inisialisasi variable
         int setoran = 500000;
-        int saldosetelahsetoran = saldo+setoran;
+        //deklarasi dan inisialisasi variable dengan menghitung jumlah saldo
+        int Saldosetelahsetoran = saldo+setoran;
+        //menampilkan setoran dan saldo setelah disetor
         System.out.println("Setoran                : "+setoran);
-        System.out.println("Saldo Setelah Setoran  : "+saldosetelahsetoran);
+        System.out.println("Saldo Setelah Setoran  : "+Saldosetelahsetoran);
+        //
         int penarikan = 250000;
-        int saldoakhir = saldosetelahsetoran-penarikan;
+        //deklarasi dan inisialisasi variable dengan menghitung jumlah saldo
+        int saldoakhir = Saldosetelahsetoran-penarikan;
+        //menampilkan penarikan dan saldo setelah ditarik
         System.out.println("Penarikan              : "+penarikan);
         System.out.println("Saldo Akhir            : "+saldoakhir);
     }
